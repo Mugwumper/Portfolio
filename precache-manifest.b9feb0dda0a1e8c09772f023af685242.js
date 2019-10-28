e9800a58bd04baced883a9f4f4ef13f395116d46
Thomas Lewis <tplewis@gmail.com>
2019-10-27 23:52:10 -0400

    Updates

---
+++ precache-manifest.b9feb0dda0a1e8c09772f023af685242.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1a861be080cd11eb1a2cb24294cac8",
+    "url": "/Port2/index.html"
+  },
+  {
+    "revision": "bffb43da08b7df7f3d67",
+    "url": "/Port2/static/css/2.eeedd492.chunk.css"
+  },
+  {
+    "revision": "25a609b7dadb03c24ca1",
+    "url": "/Port2/static/css/main.669e81e7.chunk.css"
+  },
+  {
+    "revision": "bffb43da08b7df7f3d67",
+    "url": "/Port2/static/js/2.ca1beb9c.chunk.js"
+  },
+  {
+    "revision": "25a609b7dadb03c24ca1",
+    "url": "/Port2/static/js/main.5a0386ef.chunk.js"
+  },
+  {
+    "revision": "ca1e03e655aedd4cfa8c",
+    "url": "/Port2/static/js/runtime-main.bc340fa4.js"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/Port2/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/Port2/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/Port2/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "f86d9bf34b5da8610c6a218e96667b68",
+    "url": "/Port2/static/media/react-bootstrap-carousel.f86d9bf3.svg"
+  }
+]);
