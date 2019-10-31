5928b7265e699c445c40cd7d9cca3167ba415bf0
Thomas Lewis <tplewis@gmail.com>
2019-10-30 21:21:33 -0400

    updated Main.js

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -3,8 +3,13 @@ import React from 'react'
 function Main() {
     return (
         <div style={setStyle_Main}>
-            <h1>Main</h1> 
-            <p>This is the main page!</p> 
+          <h1>About</h1> 
+          <p>Born and raised in Miami I've lived in Raleigh for most of my career.</p> 
+          <p>I have worked on some amazing teams been part of successful waterfall style projects and have come to embrace the value of Agile.</p> 
+          <p>I've worked remote, and as sole developer for a time as well. I know the value of communications and written documentation in all settings.</p> 
+          <p>I am currently seeking a role in a remote position doing whatever needs to be done to get a project large or small completed. Meaning if I don't know the technology I'm willing to learn it.</p> 
+          <p>My strengths are in writing code such as C#, Javascript, Python, database admin and in attention to detail, particularly where it concerns the user experiance.</p> 
+          <p>This portfolio represents my latest effort to branch out into the world of web development as most of my previous work has been in Windows Application development which of course is rarely needed today.</p> 
         </div>
     )
 }
